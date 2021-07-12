a153c68479056b68e9fde3bd79560ae57549354b
fgoksen <fgoksen@ascensionda.com>
2021-07-12 21:16:23 +0300

    Change part6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http,Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 //import { map } from 'rxjs/operators';
-import { map } from 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Product } from './product';
 
@@ -19,11 +19,11 @@ export class ProductService {
 
   constructor(private _http:Http) { }
 
-  getAlbum(id:number):Observable<Album>{
-    return this._http.get(this._albumUrl).pipe(map((response) => <Album>response.json()));
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
-  getProducts():Observable<Product[]>{
-    return this._http.get(this._productsUrl).pipe(map((response) => <Product[]>response.json()));
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 }
